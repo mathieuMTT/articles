38b9ce1b0f6ae481a8639432e861fd959af0479c
mathieuMTT <87305731+mathieuMTT@users.noreply.github.com>
2022-05-18 18:09:19 +0200

    Update tableau_to_slack_dau.sql

--- data_alerting/tableau_to_slack_dau.sql
+++ data_alerting/tableau_to_slack_dau.sql
@@ -1,3 +1,4 @@
+--data-processing/processing/dags/tableau_to_slack/alerting/dau.sql
 SELECT logical_or(cnt < lower_bound_scale_1_5 OR cnt > upper_bound_scale_1_5)
 FROM `{{ var.value.gcp_project_name }}.tableau.alerting_dau`
 WHERE date = '{{ ds }}'
